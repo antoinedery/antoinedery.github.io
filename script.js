cb879ef20bb9ad9ac1c98ce73ec6ecce50def67b
Antoine <antoinedery12@hotmail.fr>
2022-01-23 23:15:06 -0500

    french version added

---
+++ script.js
@@ -0,0 +1,39 @@
+function selectLanguage() {
+    if(navigator.language == "fr") 
+        window.location.replace("fr/index.html"); 
+    else
+        window.location.replace("en/index.html"); 
+
+}
+
+function selectNav(id) {
+    var slides = document.getElementsByClassName("selectedNav");
+    for (var i = 0; i < slides.length; i++) 
+        slides.item(i).classList.remove("selectedNav")
+    
+    if(slides.length == 0)
+        document.getElementById(id).click
+    document.getElementById(id).classList.add("selectedNav");
+}
+
+function selectLanguageEn() {
+    window.location.href='/en/#home'
+    var slides = document.getElementsByClassName("selectedLanguage");
+    for (var i = 0; i < slides.length; i++) 
+        slides.item(i).classList.remove("selectedLanguage")
+    
+    if(slides.length == 0)
+        document.getElementById('en').click
+    document.getElementById('en').classList.add("selectedLanguage");
+}
+
+function selectLanguageFr() {
+    window.location.href='/fr/#home'
+    var slides = document.getElementsByClassName("selectedLanguage");
+    for (var i = 0; i < slides.length; i++) 
+        slides.item(i).classList.remove("selectedLanguage")
+    
+    if(slides.length == 0)
+        document.getElementById('fr').click
+    document.getElementById('fr').classList.add("selectedLanguage");
+}
